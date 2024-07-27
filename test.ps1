1c9859dd6d6097106e12bfaf9c2f937c9b5a1c48
Andy Pfister <andy.pfister@hey.com>
2024-07-27 16:59:04 +0200

    Support `force encryption` for the MSSQL server

--- test.ps1
+++ test.ps1
@@ -8,3 +8,34 @@ else {
 
 Write-Output "Checking if SQL Server is available ..."
 & sqlcmd -S 127.0.0.1 -U sa -P $env:SA_PASSWORD -Q "SELECT 1"
+
+Write-Output "Check status of connection encryption ..."
+
+$sqlQuery = @"
+SELECT 
+session_id,
+encrypt_option
+FROM sys.dm_exec_connections
+WHERE session_id = @@SPID;
+"@
+
+$results = sqlcmd -S 127.0.0.1 -U sa -P $env:SA_PASSWORD -Q $sqlQuery -h -1 -W
+
+if ($env:FORCE_ENCRYPTION -eq "true") {
+    if ($results -match "TRUE") {
+        Write-Output "Connection from sqlcmd to the sqlengine appears to be encrypted, as expected!"
+    }
+    else {
+        Write-Error "Connection to SQL server is not encrypted!"
+        exit 1
+    }
+}
+else {
+    if ($results -match "TRUE") {
+        Write-Error "Somehow the connection to the SQL server is encrypted, misconfiguration?"
+        exit 1
+    }
+    else {
+        Write-Output "Connection from sqlcmd to the sqlengine appears to not be encrypted, as expected!"
+    }
+}
