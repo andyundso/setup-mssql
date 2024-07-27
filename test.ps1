74b23558ffe348a929aba62b7a080dc14d68acf1
Andy Pfister <andy.pfister@hey.com>
2024-07-27 14:06:12 +0200

    Support installation of SQL Express 2017

--- test.ps1
+++ test.ps1
@@ -1,6 +1,10 @@
 if (Get-Command sqlcmd -ErrorAction SilentlyContinue) {
     Write-Output "sqlcmd command exists."
-} else {
+}
+else {
     Write-Output "sqlcmd command does not exist."
     exit 1
 }
+
+Write-Output "Checking if SQL Server is available ..."
+& sqlcmd -S 127.0.0.1 -U sa -P $env:SA_PASSWORD -Q "SELECT 1"
